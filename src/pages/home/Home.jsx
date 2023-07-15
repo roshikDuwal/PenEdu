81cc4515fc07719ccbc7d390a639a773d8db0205
susankoju <aksusankoju@gmail.com>
2023-07-15 09:04:08 +0545

    Update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,36 +1,46 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import "./home.scss"
-import Herosection from "../../components/herosection/Herosection"
+import React, { useEffect } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "./home.scss";
+import Herosection from "../../components/herosection/Herosection";
 
 const Home = () => {
-
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("user", "{}"))?.token;
+    if (token) {
+      navigate("/dashboard");
+    }
+  }, []);
   const data = {
     name: "Better Education",
-    description: "PEN Clinic targets your weakness , directing your time and energy into specialised practice to achieve academic success",
+    description:
+      "PEN Clinic targets your weakness , directing your time and energy into specialised practice to achieve academic success",
     image: "/hero.jpg",
-    button: "Contact Us"
-  }
+    button: "Contact Us",
+  };
   return (
     <>
-      <nav className='homenav'>
+      <nav className="homenav">
         <div className="logo">
           <img src="/logo.png" alt="" />
         </div>
         <div className="navbar">
           <ul>
-            <li><NavLink to="/">Home</NavLink></li>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
             <li>Course</li>
             <li>About Us</li>
-            <li><NavLink to="/login">Login</NavLink></li>
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
           </ul>
         </div>
       </nav>
 
       <Herosection data={data} />
-
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,26 +1,30 @@
-import axios from 'axios';
-import { SERVER_BASE_URL } from '../constants/url';
+import axios from "axios";
+import { SERVER_BASE_URL } from "../constants/url";
 
 export class Axios {
-    constructor(config) {
-        const a = axios.create({
-            baseURL: SERVER_BASE_URL,
-        })
-        a.interceptors.response.use(response => {
-            return response;
-        }, error => {
-            if (error.response.status === 401) {
-            window.location.replace("/login");
-            }
-            throw error;
-        });
-        a.interceptors.request.use(async (request) => {
-            const user = JSON.parse(localStorage.getItem("user", "{}"));
-            if(user?.token)
-                request.headers['Authorization'] = `Bearer ${user?.token}`;
+  constructor(config) {
+    const a = axios.create({
+      baseURL: SERVER_BASE_URL,
+    });
+    a.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        if (error.response.status === 401) {
+          localStorage.removeItem("user");
+          window.location.replace("/login");
+        }
+        throw error;
+      }
+    );
+    a.interceptors.request.use(async (request) => {
+      const user = JSON.parse(localStorage.getItem("user", "{}"));
+      if (user?.token)
+        request.headers["Authorization"] = `Bearer ${user?.token}`;
 
-            return request;
-        })
-        return a;
-    }
+      return request;
+    });
+    return a;
+  }
 }
